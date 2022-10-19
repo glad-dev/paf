1523bc694dd8fd24cc6a5e17ad70ec08ac32c7bc
Gabriel Loyola Daiqui <gabriel@loyola.dev>
2022-10-19 19:49:55 +0200

    Added version number

--- flags.go
+++ flags.go
@@ -9,22 +9,31 @@ import (
 type config struct {
 	abstract string
 	help     bool
+	version  bool
 }
 
 func parseFlags() (config, error) {
 	conf := config{
 		abstract: "",
 		help:     false,
+		version:  false,
 	}
 
 	flag.BoolVar(&conf.help, "h", false, "Show help")
+	flag.BoolVar(&conf.version, "v", false, "Show the version of paf")
 
 	flag.Parse()
 	args := flag.Args()
 
 	if conf.help {
-		fmt.Print("Usage: paf {abstract}\n\n")
-		fmt.Println("Formats the passed abstract to be a valid BibTeX value.")
+		fmt.Print("Usage: paf {abstract} [-h] [-v]\n\n")
+		fmt.Print("Formats the passed abstract to be a valid BibTeX value.\n\n")
+		fmt.Println("Optional parameter:")
+		fmt.Println("  -h\tDisplays this help message and exits")
+		fmt.Println("  -v\tDisplays program's version number and exists")
+		os.Exit(0)
+	} else if conf.version {
+		fmt.Println("paf 0.2")
 		os.Exit(0)
 	}
 
